550378fc59ecf04f1c34cc6927f61b05e6426941
alexeyinn <alexey.i.nn@gmail.com>
2021-11-24 16:36:36 +0300

    markup: сверстал header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 function App() {
   return (
-    <div className="App">
-      <h1>Hello, World!</h1>
+    <div className="App clear">
+      <header className="d-flex justify-between align-center p-40">
+        <div className="headerLeft d-flex align-center">
+          <img src="img/logo.png" alt="logo" />
+          <div className="headerInfo">
+            <h3 className="text-uppercase">React Sneakers</h3>
+            <p>Магазин лучших кросовок</p>
+          </div>
+        </div>
+        <ul className="d-flex">
+          <li className="mr-30">
+            <img src="img/cart.svg" alt="cart" />
+            <span>1205 руб.</span>
+          </li>
+          <li>
+            <img src="img/user.svg" alt="user" />
+          </li>
+        </ul>
+      </header>
+      <div className="content p-40">
+        <h1>Все кроссовки</h1>. . .
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.scss";
 import App from "./App";
+
+import "./index.scss";
+import "macro-css";
 
 ReactDOM.render(
   <React.StrictMode>
