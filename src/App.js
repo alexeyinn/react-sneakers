dbb6ebcb8a19cfe148dfb969d3954022e6b8983e
alexeyinn <alexey.i.nn@gmail.com>
2021-11-26 19:02:29 +0300

    markup: сверстали нижнюю часть side-block корзины

--- src/App.js
+++ src/App.js
@@ -4,14 +4,41 @@ function App() {
       <div className="App clear">
         <div className="overlay">
           <div className="drawer">
-            <h2 className="mb-30">Корзина</h2>
-            <div className="cartItem d-flex align-center mb-20">
-              <img className="mr-20" src="/img/sneakers/1.jpg" alt="sneakers" />
-              <div className="mr-20">
-                <p className="mb-5">Мужские кроссовки Nike Air Max 270</p>
-                <b>12 999 руб.</b>
+            <h2 className="mb-30 d-flex justify-between">
+              Корзина
+              <img className="cu-p" src="/img/btn-remove.svg" alt="remove" />
+            </h2>
+            <div className="items">
+              <div className="cartItem d-flex align-center mb-20">
+                <img
+                  className="mr-20"
+                  src="/img/sneakers/1.jpg"
+                  alt="sneakers"
+                />
+                <div className="mr-20">
+                  <p className="mb-5">Мужские кроссовки Nike Air Max 270</p>
+                  <b>12 999 руб.</b>
+                </div>
+                <img src="/img/btn-remove.svg" alt="remove" />
               </div>
-              <img src="/img/btn-remove.svg" alt="remove" />
+            </div>
+            <div className="cartTotalBlock">
+              <ul>
+                <li>
+                  <span>Итого:</span>
+                  <div></div>
+                  <b>21 498 руб. </b>
+                </li>
+                <li>
+                  <span>Налог 8%</span>
+                  <div></div>
+                  <b>1074 руб.</b>
+                </li>
+              </ul>
+              <button className="greenButton">
+                Оформить заказ
+                <img src="/img/arrow.svg" alt="arrow" />
+              </button>
             </div>
           </div>
         </div>
